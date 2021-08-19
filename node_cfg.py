c76d4026b6d201886b9a26dcd8abff728e752c73
Peter Morneau <63618040+morneaup@users.noreply.github.com>
2021-08-18 21:29:31 -0400

    Updated configuration

--- node_cfg.py
+++ node_cfg.py
@@ -1,5 +1,4 @@
 import csv
-import os
 
 # Define Node Source File
 source_file = 'nodes.csv'
@@ -18,20 +17,34 @@ with open(source_file, newline='') as csvfile:
         loopback0s.append(loopback)
         tunnel0s.append(tunnel0)
 
+print('Node Generation Script')
+print('-' * 55)
+
 # Validate Node number
 while True:
-    node_value = input('Please enter a NodeID# (1-122): ')
     try:
+        node_value = input('Please enter a Node ID# (1-122): ')
+        if 0 >= int(node_value) < 122:
+            raise ValueError
         node_idx = int(node_value) - 1
         break
     except ValueError:
-        print('Please enter a valid number! ')
 
+        print('Warning: Not valid node number! ')
+
+# Write Node Configuration
 dst_file = ('node' + node_value + '.cfg')
-
-
 with open(dst_file, 'a') as f:
  f.write(f'hostname NODE{nodes[node_idx]}\n')
+ f.write('interface loopback 0\n')
+ f.write(f' ip address {loopback0s[node_idx]} 255.255.255.255\n')
  f.write('interface tunnel 0\n')
  f.write(f' ip address {tunnel0s[node_idx]} 255.255.255.252\n')
-print('Completed!') 
+
+# Print Instructions
+print('')
+print('Node configuration generated!')
+print("Run the following commands to commit changes: ")
+print('-' * 55)
+print(' copy bootflash:/guest-share/' + dst_file + ' running-config')
+print(' copy run start') 
